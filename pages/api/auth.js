5a5d3a64a84a9171c4b10a3351707c8df6e66877
muzzamilr <mmuzzamil90@gmail.com>
2023-01-10 17:58:40 +0500

    env fix

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -11,7 +11,7 @@ export default function handler(req, res) {
 			{
 				user: user,
 			},
-			process.env.NEXT_PUBLIC_JWT_KEY,
+			`${process.env.NEXT_PUBLIC_JWT_KEY}`,
 			{ expiresIn: "12h" },
 		);
 
